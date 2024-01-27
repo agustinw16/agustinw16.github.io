bf98173d23f23e684389c3c557fd8c97f5c9c10d
Veliz Juan Agustin <agustinw14@gmail.com>
2024-01-27 00:16:22 -0300

    Test: cambios para rendimiento galeria en movil 6

--- js/galleryTouch.js
+++ js/galleryTouch.js
@@ -1,11 +1,27 @@
+/*Esta funcion tiene el objetivo de identificar en cual imagen se hizo click, de esa manera, a esa imagen agregarle o quitarle 
+la clase "touch" dependiendo si se estaba abriendo o cerrando y a las demas imagenes quitarle la clase "touch" esto con el objetivo
+de que cuando se haga click sobre la imagen en un dispositivo movil la imagen quede abierta al presionar otros botones 
+y asi no relentizar las transiciones*/
 function clickImage(classPrimary) {
     const one = document.querySelector('.one');
-    one.classList.remove("touch");
     const two = document.querySelector('.two');
-    two.classList.remove("touch");
     const three = document.querySelector('.three');
-    three.classList.remove("touch");
 
-    const imageDiv = document.querySelector('.' + classPrimary);
-    imageDiv.classList.add("touch");
+    if(classPrimary == 'one'){
+        one.classList.toggle("touch");
+        two.classList.remove("touch");
+        three.classList.remove("touch");
+    }
+    if(classPrimary == 'two'){
+        two.classList.toggle("touch");
+        one.classList.remove("touch");
+        three.classList.remove("touch");
+    }
+    if(classPrimary == 'three'){
+        three.classList.toggle("touch");
+        two.classList.remove("touch");
+        one.classList.remove("touch");
+    }
+
+    
 }
